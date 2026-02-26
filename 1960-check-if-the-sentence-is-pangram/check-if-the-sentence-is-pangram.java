c3139b6e7129d3559eec72497e342751afedff85
Anupama Singeetham <anupamasingeetham@gmail.com>
2026-02-26 06:42:10 +0530

    Time: 4 ms (25.15%) | Memory: 43.1 MB (33.98%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length() < 26){
+            return false;
+        }
+        Set<Character> set = new HashSet<>();
+        for(int i = 0;i < sentence.length();i++){
+            set.add(sentence.charAt(i));
+
+        }
+        if(set.size() == 26){
+            return true;
+        }
+        return false;
+    }
+}
